54100c5315f0d600f2c25fff27076a8bc6bc5c57
Hassan Moussa <hassanmoussa61@gmail.com>
2023-07-24 01:20:07 +0300

    fixed naming of the code sent

--- forget_password_init.php
+++ forget_password_init.php
@@ -14,19 +14,19 @@ $query->store_result();
 
 if ($query->num_rows() > 0) {
 
-   $token = substr(str_shuffle('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'), 0, 10);
+   $code = substr(str_shuffle('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'), 0, 10);
 
    $expiration_date = date('Y-m-d H:i:s', strtotime('+1 hour'));
 
 
-   $insert_query = $mysqli->prepare('INSERT INTO password_reset (user_id, token, expiration_date) VALUES (?, ?, ?)');
-   $insert_query->bind_param('iss', $user_id, $token, $expiration_date);
+   $insert_query = $mysqli->prepare('insert into password_reset (user_id, code, expiration_date) VALUES (?, ?, ?)');
+   $insert_query->bind_param('iss', $user_id, $code, $expiration_date);
    $insert_query->execute();
 
         // for email sending    
         $to = $email;
         $subject = 'Password Reset Token';
-        $message = 'Your password reset token is: ' . $token;
+        $message = 'Your password reset token is: ' . $code;
         $headers = 'From: noreply@yourdomain.com' . "\r\n" .
            'Reply-To: noreply@yourdomain.com' . "\r\n" ;
            
