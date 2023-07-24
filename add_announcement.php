02ec2e0382ad25085e3052dd47669d7378b64916
Omar-Krayyem <omar.krayyem.b@gmail.com>
2023-07-24 10:02:12 +0300

    Added time column to add_announcement file <php>

--- add_announcement.php
+++ add_announcement.php
@@ -9,13 +9,12 @@ $data = json_decode($json_data,true);
 $announcement = $_POST['announcement'];
 $user_id = $_POST['user_id'];
 $classroom_id = $_POST['classroom_id'];
+$current_time = date('Y-m-d H:i:s');
 
 if ($announcement != ""){
-    $query1 = $mysqli->prepare('insert into announcements(announcement, user_id, classroom_id) values(?,?,?)');
-    $query1->bind_param('sii', $announcement, $user_id, $classroom_id);
+    $query1 = $mysqli->prepare('insert into announcements(announcement, time, user_id, classroom_id) values(?,?,?,?)');
+    $query1->bind_param('ssii', $announcement, $current_time, $user_id, $classroom_id);
     $query1->execute();
-
-    
 
     $response['status'] = "success";
     $response['message'] = "Added successfully";
@@ -28,6 +27,6 @@ else {
 
 echo json_encode($response);
 
-//    API:ADD_ASIIGNMENT:
-//    http://localhost/Google-Classroom-Clone-BE/add_assignment.php  
+//    API:ADD_ANNOUCEMENT:
+//    http://localhost/Google-Classroom-Clone-BE/add_announcement.php  
 ?>
