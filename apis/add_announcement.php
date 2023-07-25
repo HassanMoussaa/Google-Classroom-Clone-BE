fdcff55fcab93f7346b3bc5713a914a08e097ba9
Hassan Moussa <hassanmoussa61@gmail.com>
2023-07-25 22:22:51 +0300

    adjusted the add_announcement.php

--- apis/add_announcement.php
+++ apis/add_announcement.php
@@ -3,27 +3,23 @@
 include('connection.php');
 
 $response = array();
-$json_data = file_get_contents("php://input");
-$data = json_decode($json_data,true);
 
 $announcement = $_POST['announcement'];
 $user_id = $_POST['user_id'];
 $classroom_id = $_POST['classroom_id'];
 $current_time = date('Y-m-d H:i:s');
 
-if ($announcement != ""){
+if ($announcement != "" && $user_id != "" && $classroom_id != "") {
     $query1 = $mysqli->prepare('insert into announcements(announcement, time, user_id, classroom_id) values(?,?,?,?)');
     $query1->bind_param('ssii', $announcement, $current_time, $user_id, $classroom_id);
     $query1->execute();
 
     $response['status'] = "success";
     $response['message'] = "Added successfully";
-} 
-else {
+} else {
     $response['status'] = "failed";
     $response['message'] = "Missing information";
 }
-
 
 echo json_encode($response);
 
