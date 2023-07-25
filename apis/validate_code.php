54a2a892cfd8fbe23d59dbb74b1216c023ef80ea
Hassan Moussa <hassanmoussa61@gmail.com>
2023-07-25 12:58:59 +0300

    fixed validate code

--- apis/validate_code.php
+++ apis/validate_code.php
@@ -1,32 +1,23 @@
 <?php
 include('connection.php');
-// $_POST = json_decode(file_get_contents('php://input'), true);
+$_POST = json_decode(file_get_contents('php://input'), true);
 
+$code = $_POST['code'];
 
-  $code = $_POST['code'];
+$query = $mysqli->prepare('SELECT user_id FROM password_resets WHERE code = ?');
+$query->bind_param('s', $code);
+$query->execute();
+$result = $query->get_result();
 
-    $query = $mysqli->prepare('select user_id from password_resets where code = ? AND expiration_date >= NOW()');
-    $query->bind_param('s', $code);
-    $query->execute();
-    $user_id = $query -> get_result();
-
-    if ($query->num_rows() > 0) {
-
-    $query2 = $mysqli->prepare('select password from users where id= ?');
-    $query2->bind_param('i', $user_id);
-    $query2->execute();
-    
-    if ($query2->num_rows() > 0) {
-    $password = $query2 -> get_result();
+if ($result->num_rows > 0) {
     $response['status'] = 'success';
-    $response['message'] = 'code is valid and not expired.';
-    $response['password']=$password;
-    $response['user_id']=$user_id;
-    }
-
+    $response['message'] = 'Code is valid and not expired.';
+    $user_id_data = $result->fetch_assoc();
+    $response['user_id'] = $user_id_data['user_id'];
 } else {
     $response['status'] = 'error';
     $response['message'] = 'Invalid or expired code.';
 }
 
 echo json_encode($response);
+?>
