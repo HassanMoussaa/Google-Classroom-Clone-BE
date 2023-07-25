742d39c4afbf44491ac09544c18ff2aab6e95334
Omar-Krayyem <omar.krayyem.b@gmail.com>
2023-07-25 18:35:07 +0300

    Uploaded get_student_classes file <php>

---
+++ apis/get_student_classes.php
@@ -0,0 +1,24 @@
+<?php
+    include('connection.php');
+
+    $user_id = $_GET['id'];
+
+    $query = $mysqli->prepare('SELECT cl.name, cl.section, cl.subject, cl.room, u.first_name, u.last_name
+    FROM enrollments en
+    JOIN classrooms cl ON en.classroom_id = cl.id
+    JOIN users u ON en.user_id = u.id
+    WHERE en.user_id=?');
+
+    $query->bind_param('i', $user_id);
+    $query->execute();
+    $array = $query->get_result();
+    $response = [];
+    while($information = $array->fetch_assoc()){
+        $response[] = $information;
+    }
+
+   echo json_encode($response);
+
+//    API: GET_ASSIGNMENTS
+//    http://localhost/googleclone/Google-Classroom-Clone-BE/apis/get_student_classes.php?id=3
+?>
